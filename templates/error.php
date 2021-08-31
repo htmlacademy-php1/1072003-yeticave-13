4fad4e39c047840ef7807e3c591ff8c253efdadc
Nayniv <tigra8606@gmail.com>
2021-08-31 19:13:18 +0300

    fix tab

--- templates/error.php
+++ templates/error.php
@@ -1,6 +1,5 @@
 <main class="content">
-    <div class="content__main-col">
-        <header class="content__header">
-            <h2 class="content__header-text">Ошибка <?= $error; ?></h2>
-        </header>
+  <header class="content__header">
+    <h2 class="content__header-text">Ошибка <?= $error; ?></h2>
+  </header>
 </main>
